d56229b8fa6670de88679ed78bdc38d09a353aa1
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-05 03:31:34 -0400

    Finalize last corrections to complete assignment

--- js/challenge.js
+++ js/challenge.js
@@ -6,9 +6,9 @@ const plusBtn = document.getElementById('plus');
 const heartBtn = document.getElementById('heart');
 const pauseBtn = document.getElementById('pause');
 pauseBtn.innerHTML = 'pause';
-const likesUl = document.getElementById('likes');
+const likesUl = document.getElementsByClassName('likes')[0];
 const userComment = document.getElementById('comment-input');
-const commentsDiv = document.getElementById('comments');
+const commentsDiv = document.getElementById('list');
 const commentForm = document.getElementById('comment-form');
 const submitBtn = document.getElementById('submit');
 
@@ -23,9 +23,7 @@ let countUp = setInterval( function() {
 const plusOne = () => { counter.innerHTML = ++number; }
 const minusOne = () => { counter.innerHTML = --number; }
 const pause = () => {
-    let pauseState = pauseBtn.innerHTML;
-    // console.log(`1. ${pauseState}`);
-    if (pauseState === 'pause') {
+    if (pauseBtn.innerHTML === 'pause') {
         minusBtn.disabled = true;
         plusBtn.disabled = true;
         heartBtn.disabled = true;
@@ -51,7 +49,6 @@ const like = () => {
         let li = document.getElementById(nStr);
         let strArr = li.innerHTML.split(' ');
         let n = strArr.length;
-        console.log(strArr[n-2]);
         strArr[n-2] = parseInt(strArr[n-2]) + 1;
         strArr[n-1] = 'times';
         let str = strArr.join(' ');
@@ -59,11 +56,13 @@ const like = () => {
     }
 }
 
-const comment = () => {
-    let li = document.createElement('li');
-    li.innerHTML = userComment.innerHTML;
-    commentsDiv.appendChild(li);
-    userComment.innerHTML = '';
+const comment = event => {
+    event.preventDefault();
+    let p = document.createElement('p');
+    console.log(userComment.value);
+    p.innerText = userComment.value;
+    commentsDiv.appendChild(p);
+    userComment.value = '';
 }
 
 plusBtn.addEventListener('click', plusOne);
